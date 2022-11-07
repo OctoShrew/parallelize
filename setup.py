eb30c0c850d72a75c3e07d062e92076657403625
Felix Quinque <40171911+Hollyqui@users.noreply.github.com>
2022-11-07 14:27:12 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+INSTALL_REQUIRES = (HERE / "requirements.txt").read_text().splitlines()
+
+setup(
+   name='parallelize',
+   version='0.1.0',
+   author='OctoShrew Ltd.',
+   author_email='felix.quinque@octoshrew.com',
+   packages=['src'],
+   url='https://github.com/OctoShrew/parellelize',
+   license='LICENSE',
+   description='An easy to use parallelization library',
+   long_description=open('README.md').read(),
+   install_requires=INSTALL_REQUIRES,
+)
